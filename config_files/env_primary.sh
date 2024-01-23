fe3e68cfbf40aa8221dc30f6a7cc98de8150e007
Dumlu Timuralp <34840364+dumlutimuralp@users.noreply.github.com>
2024-01-23 19:34:20 +0000

    Update env_primary.sh

--- config_files/env_primary.sh
+++ config_files/env_primary.sh
@@ -1,11 +1,11 @@
 export AccountId=$(aws sts get-caller-identity --query "Account" --output text)
 
-export EnvironmentName=$(aws cloudformation describe-stacks --stack-name primary --query "Stacks[0].Parameters[?ParameterKey == 'EnvironmentName'].ParameterValue" --output text)
+export EnvironmentNamePrimary=$(aws cloudformation describe-stacks --stack-name primary --query "Stacks[0].Parameters[?ParameterKey == 'EnvironmentName'].ParameterValue" --output text)
 
-export AWS_REGION=$(aws ec2 describe-availability-zones --output text --query "AvailabilityZones[0].[RegionName]")
+export AWS_REGION_Primary=$(aws ec2 describe-availability-zones --output text --query "AvailabilityZones[0].[RegionName]")
 
-export PrivateSubnet1=$(aws cloudformation describe-stacks --stack-name awsblogstack --query "Stacks[0].Outputs[?contains(OutputKey, 'PrivateSubnet1')].OutputValue" --output text)
+export PrivateSubnet1Primary=$(aws cloudformation describe-stacks --stack-name awsblogstack --query "Stacks[0].Outputs[?contains(OutputKey, 'PrivateSubnet1')].OutputValue" --output text)
 
-export PrivateSubnet2=$(aws cloudformation describe-stacks --stack-name awsblogstack --query "Stacks[0].Outputs[?contains(OutputKey, 'PrivateSubnet2')].OutputValue" --output text)
+export PrivateSubnet2Primary=$(aws cloudformation describe-stacks --stack-name awsblogstack --query "Stacks[0].Outputs[?contains(OutputKey, 'PrivateSubnet2')].OutputValue" --output text)
 
-export Cluster1=primarycluster
+export ClusterPrimary=clusterprimary
