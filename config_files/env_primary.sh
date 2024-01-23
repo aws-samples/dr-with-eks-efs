35cc3e06a49e32335e64cd7e4da9a36011701a83
Dumlu Timuralp <34840364+dumlutimuralp@users.noreply.github.com>
2024-01-23 19:38:16 +0000

    Update env_primary.sh

--- config_files/env_primary.sh
+++ config_files/env_primary.sh
@@ -1,8 +1,8 @@
 export AccountId=$(aws sts get-caller-identity --query "Account" --output text)
 
-export EnvironmentNamePrimary=$(aws cloudformation describe-stacks --stack-name primary --query "Stacks[0].Parameters[?ParameterKey == 'EnvironmentName'].ParameterValue" --output text)
-
 export AWS_REGION_Primary=$(aws ec2 describe-availability-zones --output text --query "AvailabilityZones[0].[RegionName]")
+
+export EnvironmentNamePrimary=$(aws cloudformation describe-stacks --stack-name primary --query "Stacks[0].Parameters[?ParameterKey == 'EnvironmentName'].ParameterValue" --output text)
 
 export PrivateSubnet1Primary=$(aws cloudformation describe-stacks --stack-name awsblogstack --query "Stacks[0].Outputs[?contains(OutputKey, 'PrivateSubnet1')].OutputValue" --output text)
 
