45ffeb85cb3e9b9d8c4e26ec2bb295678580fdc8
Dumlu Timuralp <34840364+dumlutimuralp@users.noreply.github.com>
2024-01-24 12:31:10 +0000

    Update and rename primary_region_env.sh to pri_region_env.sh

--- config_files/primary_region_env.sh
+++
@@ -1,7 +0,0 @@
-export AccountId=$(aws sts get-caller-identity --query "Account" --output text)
-
-export EnvironmentNamePrimary=$(aws cloudformation describe-stacks --stack-name $CFNstack --query "Stacks[0].Parameters[?ParameterKey == 'EnvironmentName'].ParameterValue" --output text --region $AWS_REGION_PRIMARY)
-
-export PrivateSubnet1Primary=$(aws cloudformation describe-stacks --stack-name $CFNstack --query "Stacks[0].Outputs[?contains(OutputKey, 'PrivateSubnet1')].OutputValue" --output text --region $AWS_REGION_PRIMARY)
-
-export PrivateSubnet2Primary=$(aws cloudformation describe-stacks --stack-name $CFNstack --query "Stacks[0].Outputs[?contains(OutputKey, 'PrivateSubnet2')].OutputValue" --output text --region $AWS_REGION_PRIMARY)
