c590d2b2b7f6cbe481680b8e295386e5802690ff
Dumlu Timuralp <34840364+dumlutimuralp@users.noreply.github.com>
2024-01-24 12:41:12 +0000

    Update pri_region_env.sh

--- config_files/pri_region_env.sh
+++ config_files/pri_region_env.sh
@@ -1,7 +1,7 @@
 export AccountId=$(aws sts get-caller-identity --query "Account" --output text)
 
-export EnvironmentNamePrimary=$(aws cloudformation describe-stacks --stack-name $pri_cfn_name --query "Stacks[0].Parameters[?ParameterKey == 'EnvironmentName'].ParameterValue" --output text --region $AWS_REGION_PRIMARY)
+export PRI_ENVIRONMENT_NAME=$(aws cloudformation describe-stacks --stack-name $PRI_CFN_NAME --query "Stacks[0].Parameters[?ParameterKey == 'EnvironmentName'].ParameterValue" --output text --region $PRI_REGION)
 
-export PrivateSubnet1Primary=$(aws cloudformation describe-stacks --stack-name $pri_cfn_name --query "Stacks[0].Outputs[?contains(OutputKey, 'PrivateSubnet1')].OutputValue" --output text --region $AWS_REGION_PRIMARY)
+export PRI_PRIVATE_SUBNET_1=$(aws cloudformation describe-stacks --stack-name $PRI_CFN_NAME --query "Stacks[0].Outputs[?contains(OutputKey, 'PrivateSubnet1')].OutputValue" --output text --region $$PRI_REGION)
 
-export PrivateSubnet2Primary=$(aws cloudformation describe-stacks --stack-name $pri_cfn_name --query "Stacks[0].Outputs[?contains(OutputKey, 'PrivateSubnet2')].OutputValue" --output text --region $AWS_REGION_PRIMARY)
+export PRI_PRIVATE_SUBNET_2=$(aws cloudformation describe-stacks --stack-name $PRI_CFN_NAME --query "Stacks[0].Outputs[?contains(OutputKey, 'PrivateSubnet2')].OutputValue" --output text --region $PRI_REGION)
