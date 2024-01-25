031ed46862d116c4704abfa24dd3fde4d5fc2288
Dumlu Timuralp <34840364+dumlutimuralp@users.noreply.github.com>
2024-01-25 09:43:11 +0000

    Update pri_region_env.sh

--- config_files/pri_region_env.sh
+++ config_files/pri_region_env.sh
@@ -5,3 +5,5 @@ export PRI_ENVIRONMENT_NAME=$(aws cloudformation describe-stacks --stack-name $P
 export PRI_PRIVATE_SUBNET_1=$(aws cloudformation describe-stacks --stack-name $PRI_CFN_NAME --query "Stacks[0].Outputs[?contains(OutputKey, 'PrivateSubnet1')].OutputValue" --output text --region $PRI_REGION)
 
 export PRI_PRIVATE_SUBNET_2=$(aws cloudformation describe-stacks --stack-name $PRI_CFN_NAME --query "Stacks[0].Outputs[?contains(OutputKey, 'PrivateSubnet2')].OutputValue" --output text --region $PRI_REGION)
+
+export PRI_EFS_ID=$(aws cloudformation describe-stacks --stack-name $PRI_CFN_NAME --query "Stacks[0].Outputs[?contains(OutputKey, 'EFS')].OutputValue" --output text --region $PRI_REGION)
