d3e6d8df58080c6b7afbd3f853d0a528284fee67
Dumlu Timuralp <34840364+dumlutimuralp@users.noreply.github.com>
2024-01-25 09:42:18 +0000

    Update dr_region_env.sh

--- config_files/dr_region_env.sh
+++ config_files/dr_region_env.sh
@@ -5,3 +5,5 @@ export DR_ENVIRONMENT_NAME=$(aws cloudformation describe-stacks --stack-name $DR
 export DR_PRIVATE_SUBNET_1=$(aws cloudformation describe-stacks --stack-name $DR_CFN_NAME --query "Stacks[0].Outputs[?contains(OutputKey, 'PrivateSubnet1')].OutputValue" --output text --region $DR_REGION)
 
 export DR_PRIVATE_SUBNET_2=$(aws cloudformation describe-stacks --stack-name $DR_CFN_NAME --query "Stacks[0].Outputs[?contains(OutputKey, 'PrivateSubnet2')].OutputValue" --output text --region $DR_REGION)
+
+export DR_EFS_ID=$(aws cloudformation describe-stacks --stack-name $DR_CFN_NAME --query "Stacks[0].Outputs[?contains(OutputKey, 'EFS')].OutputValue" --output text --region $DR_REGION)
