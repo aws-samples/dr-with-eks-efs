d938ecaca8e7d1bde281dbf4e93c62f417dd97ff
Dumlu Timuralp <34840364+dumlutimuralp@users.noreply.github.com>
2024-10-17 13:21:53 +0100

    Update startup.sh

--- workshop/startup.sh
+++ workshop/startup.sh
@@ -3,6 +3,11 @@
 # Install gettext
 sudo yum install -y gettext
 
+# Install kubectx
+sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
+sudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
+sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
+
 # Install eskctl
 curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_Linux_amd64.tar.gz"
 tar -xzf eksctl_Linux_amd64.tar.gz -C /tmp
@@ -13,39 +18,21 @@ sudo mv /tmp/eksctl /usr/local/bin
 aws eks update-kubeconfig --name dr --region us-east-1
 aws eks update-kubeconfig --name primary --region us-west-2
 
-
-
-# Clone workshop repo
-
-git clone https://github.com/aws-samples/dr-with-eks-efs.git
-cd dr-with-eks-efs/workshop
-
-
-
 # Function to add or update an environment variable in .bashrc
 add_or_update_env_var() {
     local var_name=$1
     local var_value=$2
     local bashrc_file=~/.bashrc
-
-    # Check if the variable already exists in .bashrc
-    if grep -q "export $var_name=" "$bashrc_file"; then
-        # Update existing variable
-        sed -i "s|export $var_name=.*|export $var_name=\"$var_value\"|" "$bashrc_file"
-    else
-        # Add new variable
-        echo "export $var_name=\"$var_value\"" >> "$bashrc_file"
-    fi
+    sed -i "/export $var_name=/d" "$bashrc_file"
+    echo "export $var_name=\"$var_value\"" >> "$bashrc_file"
 }
 
 # Add your environment variables here
-add_or_update_env_var
-
-add_or_update_env_var "MY_VARIABLE" "my_value"
 add_or_update_env_var "AWS_DEFAULT_REGION" "us-west-2"
+add_or_update_env_var "PRI_REGION" "us-west-2"
 add_or_update_env_var "PRI_CLUSTER_NAME" "primary"
+add_or_update_env_var "PRI_ENVIRONMENT_NAME" "primary"
 add_or_update_env_var "DR_CLUSTER_NAME" "dr"
-
 
 # Add more variables as needed
 # add_or_update_env_var "ANOTHER_VARIABLE" "another_value"
@@ -60,3 +47,9 @@ echo "Changes have been applied to the current session"
 # Optionally, print the new variables to verify
 echo "MY_VARIABLE is now set to: $MY_VARIABLE"
 echo "AWS_DEFAULT_REGION is now set to: $AWS_DEFAULT_REGION"
+
+
+# Clone GitHub repo for the workshop
+
+git clone https://github.com/aws-samples/dr-with-eks-efs.git
+cd dr-with-eks-efs/workshop
